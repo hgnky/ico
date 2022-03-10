88c6a1520e1f1e16d8a902eaeceb8bfe72f09435
baba <gordofab94@gmail.com>
2022-03-10 14:19:53 +0100

    up

--- src/components/PresaleCard/index.tsx
+++ src/components/PresaleCard/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { ReactComponent as HeartIcon } from '../../../assets/img/heart.svg';
+import { Link } from 'react-router-dom';
+import { routeNames } from 'routes';
+import EclairConnectBlack from './../../assets/img/eclair_black.png';
 
-class PresaleCard extends React.Component {
+class PresaleCard extends React.Component<any, any> {
   render() {
     return (
       <div className='col mb-5'>
         <div className='card h-80 radius'>
           <div className='badge bg-dark text-white position-absolute'>
-            Round 1
+            {this.props.round}
           </div>
           <span className='token_pack'>
             1,000,000,000
             <span className='vital_token'></span>
           </span>
-          <span className='supply'>Supply 3,334,000,000,000</span>
+          <span className='supply'>Supply {this.props.supply}</span>
           <div className='card-body p-4'>
             <div className='text-center'>
               <span className='minus'>
@@ -27,9 +30,23 @@ class PresaleCard extends React.Component {
           </div>
           <div className='card-footer p-4 pt-0 border-top-0 bg-transparent'>
             <div className='text-center'>
-              <a className='btn mt-auto' href='#'>
-                Buy 0.75 EGLD
-              </a>
+              {this.props.isLogged ? (
+                <Link
+                  to={routeNames.unlock}
+                  className='btn connect mt-3'
+                  data-testid='loginBtn'
+                >
+                  <span>Buy {this.props.egldprice}</span>
+                </Link>
+              ) : (
+                <Link
+                  to={routeNames.unlock}
+                  className='btn connect mt-3'
+                  data-testid='loginBtn'
+                >
+                  <span>Connect</span>
+                </Link>
+              )}
             </div>
           </div>
           <p>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,53 +3,71 @@ import { Link } from 'react-router-dom';
 import { dAppName } from 'config';
 import { routeNames } from 'routes';
 import PresaleCard from '../../components/PresaleCard';
+import { useGetAccountInfo, DappUI } from '@elrondnetwork/dapp-core';
 
-class Home extends React.Component {
-  render() {
-    return (
+const Home = () => {
+  const { address } = useGetAccountInfo();
+
+  const isLoggedIn = Boolean(address);
+
+  return (
+    <div>
       <div>
-        <div>
-          <div className='d-flex flex-fill container'>
-            <div className='row presentation-block'>
-              <div className='col-12 col-12-title'>
-                <h1>
-                  <span className='vital_token'></span> VITAL ICO
-                </h1>
-                <p>
-                  Initial Coin Offering (ICO) VITAL
-                  <br />
-                  Supply 10,000,000,000,000
-                </p>
-                <p>Remaining supply</p>
-                <span className='token_remain'>
-                  8,568,000,000,000
-                  <span className='vital_token'></span>
-                </span>
-                <div className='progress'>
-                  <div
-                    className='progress-bar progress-round progress-bar-striped progress-bar-animated'
-                    role='progressbar'
-                    aria-valuenow={75}
-                    aria-valuemin={0}
-                    aria-valuemax={100}
-                  >
-                    30%
-                  </div>
+        <div className='d-flex flex-fill container'>
+          <div className='row presentation-block'>
+            <div className='col-12 col-12-title'>
+              <h1>
+                <span className='vital_token'></span> VITAL ICO
+              </h1>
+              <p>
+                Initial Coin Offering (ICO) VITAL
+                <br />
+                Supply 10,000,000,000,000
+              </p>
+              <p>Remaining supply</p>
+              <span className='token_remain'>
+                8,568,000,000,000
+                <span className='vital_token'></span>
+              </span>
+              <div className='progress'>
+                <div
+                  className='progress-bar progress-round progress-bar-striped progress-bar-animated'
+                  role='progressbar'
+                  aria-valuenow={75}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                >
+                  30%
                 </div>
               </div>
             </div>
           </div>
-          <div className='container'>
-            <div className='row'>
-              <PresaleCard supply='1000' />
-              <PresaleCard />
-              <PresaleCard />
-            </div>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <PresaleCard
+              isLogged={isLoggedIn}
+              round='Round 1'
+              supply='3,334,000,000,000'
+              egldprice='0.75 EGLD'
+            />
+            <PresaleCard
+              isLogged={isLoggedIn}
+              round='Round 2'
+              supply='3,333,000,000,000'
+              egldprice='1.15 EGLD'
+            />
+            <PresaleCard
+              isLogged={isLoggedIn}
+              round='Round 3'
+              supply='3,333,000,000,000'
+              egldprice='1.45 EGLD'
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
